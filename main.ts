c35a7ad9ec91cddb50a402f365157078449f991a
IZUMO2019 <51372810+IZUMO2019@users.noreply.github.com>
2022-07-31 10:42:34 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+let x = 0
+basic.showNumber(x)
+basic.pause(1000)
+basic.forever(function () {
+    x += 1
+    basic.showNumber(x)
+    basic.pause(1000)
+})
